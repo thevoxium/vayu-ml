b51ace207a9c5bc51c54ad514e3b3b03fe98b611
thevoxium <thevoxium@gmail.com>
2025-06-13 14:00:00 +0530

    wrote value.h

---
+++ include/value.h
@@ -0,0 +1,37 @@
+#ifndef VALUE_H
+#define VALUE_H
+
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <ostream>
+#include <set>
+#include <string>
+#include <vector>
+
+class Value : public std::enable_shared_from_this<Value> {
+public:
+  double data;
+  double grad;
+  std::function<void()> _backward;
+  std::set<std::shared_ptr<Value>> _prev;
+  std::string _op, label;
+
+  Value(double data, std::set<std::shared_ptr<Value>> children = {},
+        std::string op = "");
+
+  std::shared_ptr<Value> operator+(std::shared_ptr<Value> other);
+  std::shared_ptr<Value> operator*(std::shared_ptr<Value> other);
+  std::shared_ptr<Value> pow(double other);
+  std::shared_ptr<Value> relu();
+  std::shared_ptr<Value> operator-(std::shared_ptr<Value> other);
+  std::shared_ptr<Value> operator/(std::shared_ptr<Value> other);
+
+  void backward();
+
+  friend std::ostream &operator<<(std::ostream &os, const Value &v);
+};
+
+std::shared_ptr<Value> make_value(double data);
+
+#endif // VALUE_H
