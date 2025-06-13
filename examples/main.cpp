73e5e6be81c5d9d4c9e15773e863b4848c28f058
thevoxium <thevoxium@gmail.com>
2025-06-14 05:12:32 +0530

    added gradient descent in main file

--- examples/main.cpp
+++ examples/main.cpp
@@ -2,18 +2,30 @@
 #include <iostream>
 
 int main() {
-  auto a = make_value(2.0);
-  auto b = make_value(-3.0);
-  auto c = pow(a, 3) - b;
-  auto d = c / b;
-  auto e = -d;
+  auto w = make_value(0.0);
+  auto b = make_value(0.0);
+  double learning_rate = 0.0001;
 
-  e->backward();
+  double x_data = 3.0, y_data = 5.0;
 
-  std::cout << "a: " << *a << std::endl;
-  std::cout << "b: " << *b << std::endl;
-  std::cout << "c: " << *c << std::endl;
-  std::cout << "d: " << *d << std::endl;
-  std::cout << "e: " << *e << std::endl;
+  for (int i = 0; i < 10000; i++) {
+    auto x = make_value(x_data);
+    auto y_true = make_value(y_data);
+    auto y_pred = w * x + b;
+    auto loss = (y_pred - y_true);
+    auto loss_sq = pow(loss, 2);
+
+    loss_sq->backward();
+
+    w = make_value(w->data - learning_rate * w->grad);
+    b = make_value(b->data - learning_rate * b->grad);
+
+    w->grad = 0.0;
+    b->grad = 0.0;
+  }
+
+  std::cout << "Trained w: " << *w << std::endl;
+  std::cout << "Trained b: " << *b << std::endl;
+
   return 0;
 }
