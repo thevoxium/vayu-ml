3c121840574607948735cfe2e4654b5a5d075733
thevoxium <thevoxium@gmail.com>
2025-06-16 15:05:23 +0530

    Added sq

--- src/activations.cpp
+++ src/activations.cpp
@@ -9,7 +9,7 @@ void Relu::zero_grad() {}
 std::vector<std::shared_ptr<Tensor>> Relu::parameters() { return {}; }
 
 std::shared_ptr<Tensor> Sigmoid::forward(std::shared_ptr<Tensor> input) {
-  return input->relu();
+  return input->sigmoid();
 }
 void Sigmoid::zero_grad() {}
 std::vector<std::shared_ptr<Tensor>> Sigmoid::parameters() { return {}; }
