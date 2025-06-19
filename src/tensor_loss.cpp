04aada7bc680c50afa2ae11522e7c4cc5c68bacb
thevoxium <thevoxium@gmail.com>
2025-06-20 00:21:45 +0530

    added backprop to cce loss

--- src/tensor_loss.cpp
+++ src/tensor_loss.cpp
@@ -11,8 +11,8 @@ std::shared_ptr<Tensor> Tensor::mse_loss(std::shared_ptr<Tensor> target) {
   auto diff = self_ptr - target;
   auto diff_sq = diff * diff;
   auto sum_loss = diff_sq->sum();
-  auto out =
-      std::make_shared<Tensor>(std::vector<size_t>{1}, sum_loss->requires_grad);
+  auto out = std::make_shared<Tensor>(std::vector<size_t>{1, 1},
+                                      sum_loss->requires_grad);
   float n = static_cast<float>(this->numel());
   out->data[0] = sum_loss->data[0] / n;
   out->_prev = {sum_loss};
@@ -35,7 +35,7 @@ std::shared_ptr<Tensor>
 Tensor::cross_entropy_loss(std::shared_ptr<Tensor> target) {
   assert(this->shape == target->shape);
   auto out = std::make_shared<Tensor>(
-      std::vector<size_t>{1}, this->requires_grad || target->requires_grad);
+      std::vector<size_t>{1, 1}, this->requires_grad || target->requires_grad);
   size_t batch_size = this->shape[0], num_classes = this->shape[1];
   float sum_loss = 0.0f;
   for (size_t b = 0; b < batch_size; b++) {
@@ -48,7 +48,19 @@ Tensor::cross_entropy_loss(std::shared_ptr<Tensor> target) {
   out->_prev = {shared_from_this(), target};
   out->_op = "cross_entropy_loss";
   auto self_ptr = shared_from_this();
-
+  out->_backward = [self_ptr, target, batch_size, num_classes, out]() {
+    if (self_ptr->requires_grad) {
+      for (size_t b = 0; b < batch_size; b++) {
+        for (size_t i = 0; i < num_classes; i++) {
+          size_t idx = b * num_classes + i;
+          self_ptr->grad[idx] +=
+              out->grad[0] *
+              (-target->data[idx] / (self_ptr->data[idx] + 1e-9f)) /
+              static_cast<float>(batch_size);
+        }
+      }
+    }
+  };
   out->_backward = [self_ptr, target, batch_size, num_classes]() {
 
   };
